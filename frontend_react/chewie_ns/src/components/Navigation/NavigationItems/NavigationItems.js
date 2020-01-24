609934c3d137a57f61ba3adb2f59ca86cc4b4711
pedrorvc <pedrorvc@gmail.com>
2020-01-24 11:37:48 +0000

    Added 'noopener noreferrer' to the rel prop of the IconButton that opens the Github repo.

--- frontend_react/chewie_ns/src/components/Navigation/NavigationItems/NavigationItems.js
+++ frontend_react/chewie_ns/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -24,7 +24,7 @@ const navigationItems = props => (
     <IconButton
       href={"https://github.com/B-UMMI/Nomenclature_Server_docker_compose"}
       target={"_blank"}
-      rel="noreferrer" // Check --> https://material-ui.com/components/links/#security
+      rel="noopener noreferrer" // Check --> https://material-ui.com/components/links/#security
     >
       <GitHubIcon fontSize="large" color="action"/>
     </IconButton>
