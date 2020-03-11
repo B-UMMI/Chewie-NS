36ca5181b87ddb2159cae734f299076269801dd2
pedrorvc <pedrorvc@gmail.com>
2020-03-11 22:02:47 +0000

    Modified styling, commented old side drawer code and added the new Material UI side drawer.

--- frontend_react/chewie_ns/src/hoc/Layout/Layout.js
+++ frontend_react/chewie_ns/src/hoc/Layout/Layout.js
@@ -3,35 +3,37 @@ import { connect } from 'react-redux';
 
 import Aux from "../Aux/Aux";
 import classes from "./Layout.module.css";
-import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+// import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+// import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import MuiSideDrawer from "../../components/Navigation/MuiSideDrawer/MuiSideDrawer";
 
 class Layout extends Component {
-  state = {
-    showSideDrawer: false
-  }
+  // state = {
+  //   showSideDrawer: false
+  // }
 
-  sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false});
-  }
+  // sideDrawerClosedHandler = () => {
+  //   this.setState({showSideDrawer: false});
+  // }
 
-  // Set state when it depends on the old state
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return {showSideDrawer: !this.state.showSideDrawer};
-    } );
-  }
+  // // Set state when it depends on the old state
+  // sideDrawerToggleHandler = () => {
+  //   this.setState((prevState) => {
+  //     return {showSideDrawer: !this.state.showSideDrawer};
+  //   } );
+  // }
 
   render() {
     return (
       <Aux>
-        <Toolbar 
+        {/* <Toolbar 
           isAuth={this.props.isAuthenticated}
           drawerToggleClicked={this.sideDrawerToggleHandler}/>
         <SideDrawer
           isAuth={this.props.isAuthenticated} 
           open={this.state.showSideDrawer}
-          closed={this.sideDrawerClosedHandler} />
+          closed={this.sideDrawerClosedHandler} /> */}
+        <MuiSideDrawer />
         <main className={classes.Content}>
           {this.props.children}</main>
       </Aux>
