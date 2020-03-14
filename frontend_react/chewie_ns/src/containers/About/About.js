268bc530d860f072bda1eb236d19d19e3246d81c
pedrorvc <pedrorvc@gmail.com>
2020-03-14 15:07:25 +0000

    Fixed the issue with the missing space. The footer now gets the year dynamically.

--- frontend_react/chewie_ns/src/containers/About/About.js
+++ frontend_react/chewie_ns/src/containers/About/About.js
@@ -37,7 +37,8 @@ class About extends Component {
           >
             GPLv3 license
           </a>
-          . The source code of Chewie-NS is available at <a
+          . The source code of Chewie-NS is available at{" "}
+          <a
             href="https://github.com/B-UMMI/Nomenclature_Server_docker_compose"
             target={"_blank"}
             rel="noopener noreferrer"
@@ -58,8 +59,9 @@ class About extends Component {
         >
           <div id="homeFooter" style={{ display: "block" }}>
             <div>
-              <Typography style={{ fontSize: "10" }}>© UMMI 2020</Typography>
-              {/* <p>© UMMI 2020</p> */}
+              <Typography style={{ fontSize: "10" }}>
+                © UMMI {new Date().getFullYear()}
+              </Typography>
             </div>
           </div>
         </footer>
