75aeff8d6d1eca8bf0a35b50c959505f5d24490f
pedrorvc <pedrorvc@gmail.com>
2020-05-22 13:38:56 +0100

    Added headers to the axios instance. Headers will alwyas enforce application/json.

--- frontend_react/chewie_ns/src/axios-backend.js
+++ frontend_react/chewie_ns/src/axios-backend.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'https://chewbbaca.online/api/NS/api'
+   baseURL: 'https://194.210.120.209/api/NS/api',
+   headers: { 'Content-Type': 'application/json' },
 });
 
 export default instance;
