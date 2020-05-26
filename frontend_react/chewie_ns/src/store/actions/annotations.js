ba75dd17684bb921d46024112542dc6bebf681cc
pedrorvc <pedrorvc@gmail.com>
2020-05-26 14:33:29 +0100

    The locus IDs are now being converted to integers to avoid alphanumerical sorting.

--- frontend_react/chewie_ns/src/store/actions/annotations.js
+++ frontend_react/chewie_ns/src/store/actions/annotations.js
@@ -1,32 +1,34 @@
 import * as actionTypes from "./actionTypes";
 import axios from "../../axios-backend";
 
-export const fetchAnnotationsSuccess = annotations => {
+export const fetchAnnotationsSuccess = (annotations) => {
   return {
     type: actionTypes.FECTH_ANNOTATIONS_SUCCESS,
-    annotations: annotations
+    annotations: annotations,
   };
 };
 
-export const fetchAnnotationsFail = error => {
+export const fetchAnnotationsFail = (error) => {
   return {
     type: actionTypes.FECTH_ANNOTATIONS_FAIL,
-    error: error
+    error: error,
   };
 };
 
 export const fetchAnnotationsStart = () => {
   return {
-    type: actionTypes.FECTH_ANNOTATIONS_START
+    type: actionTypes.FECTH_ANNOTATIONS_START,
   };
 };
 
 export const fetchAnnotations = (species_id, schema_id) => {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(fetchAnnotationsStart());
     axios
-      .get("/stats/species/" + species_id + "/schema/" + schema_id + "/annotations")
-      .then(res => {
+      .get(
+        "/stats/species/" + species_id + "/schema/" + schema_id + "/annotations"
+      )
+      .then((res) => {
         // console.log(res);
         let annotTableData = [];
         // let annotTableData2 = [];
@@ -42,17 +44,19 @@ export const fetchAnnotations = (species_id, schema_id) => {
             // schema: res.data.message[key].schema.value.substring(
             //   res.data.message[key].schema.value.lastIndexOf("/") + 1
             // ),
-            locus: res.data.message[key].locus.substring(
-              res.data.message[key].locus.lastIndexOf("/") + 1
+            locus: parseInt(
+              res.data.message[key].locus.substring(
+                res.data.message[key].locus.lastIndexOf("/") + 1
+              )
             ),
             locus_name: res.data.message[key].name,
-            alleles_mode: parseInt(res.data.message[key].mode)
+            alleles_mode: parseInt(res.data.message[key].mode),
           });
         }
         // console.log(annotTableData2)
         dispatch(fetchAnnotationsSuccess(annotTableData));
       })
-      .catch(annotErr => {
+      .catch((annotErr) => {
         dispatch(fetchAnnotationsFail(annotErr));
       });
   };
