47b39dad15a039dccbf4b0c98ef0e036359448ac
pedrorvc <pedrorvc@gmail.com>
2020-06-15 11:10:45 +0100

    Added a breadcrumb for the Search page.

--- frontend_react/chewie_ns/src/App.js
+++ frontend_react/chewie_ns/src/App.js
@@ -11,29 +11,28 @@ import { connect } from "react-redux";
 
 // Chewie Components
 import Aux from "./hoc/Aux/Aux";
-import Layout from "./hoc/Layout/Layout";
-import Chewie from "./containers/Chewie/Chewie";
+import Stats from "./containers/Stats/Stats";
 import About from "./containers/About/About";
-// import Auth from "./containers/Auth/Auth";
+import Locus from "./containers/Locus/Locus";
+import Chewie from "./containers/Chewie/Chewie";
+import Schema from "./containers/Schema/Schema";
+import Logout from "./containers/Auth/Logout/Logout";
+import Species from "./containers/Species/Species";
 import MuiLogin from "./containers/Auth/MuiLogin/MuiLogin";
+import Sequences from "./containers/Sequences/Sequences";
 import MuiRegister from "./containers/Auth/MuiRegister/MuiRegister";
 import MuiSideDrawer from "./components/Navigation/MuiSideDrawer/MuiSideDrawer";
-import Logout from "./containers/Auth/Logout/Logout";
-import Stats from "./containers/Stats/Stats";
-import Species from "./containers/Species/Species";
-import Schema from "./containers/Schema/Schema";
-import Locus from "./containers/Locus/Locus";
-import Sequences from "./containers/Sequences/Sequences";
 import * as actions from "./store/actions/index";
 
 // Material Ui Components
-import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import MuiLink from "@material-ui/core/Link";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 
 function SimpleBreadcrumbs() {
-  const homeMatches = useRouteMatch("/");
+  // const homeMatches = useRouteMatch("/");
   const aboutMatches = useRouteMatch("/about");
   const statsMatches = useRouteMatch("/stats");
+  const sequencesMatches = useRouteMatch("/sequences");
   const speciesIdMatches = useRouteMatch("/species/:species_id");
   const schemasIdMatches = useRouteMatch(
     "/species/:species_id/schemas/:schema_id"
@@ -67,6 +66,11 @@ function SimpleBreadcrumbs() {
               About
             </MuiLink>
           </div>
+        )}
+        {sequencesMatches && (
+          <MuiLink component={Link} to="/stats" style={styles.breadcrumb}>
+            Search
+          </MuiLink>
         )}
         {statsMatches && (
           <MuiLink component={Link} to="/stats" style={styles.breadcrumb}>
@@ -141,6 +145,7 @@ class App extends Component {
           <Route path="/logout" component={Logout} />
           <Route path="/about" component={About} />
           <Route path="/stats" component={Stats} />
+          <Route path="/sequences" component={Sequences} />
           <Route path="/" exact component={Chewie} />
           <Route
             path="/species/:species_id/schemas/:schema_id/locus/:locus_id"
@@ -158,7 +163,6 @@ class App extends Component {
 
     return (
       <div>
-        {/* <Layout>{routes}</Layout> */}
         <MuiSideDrawer>{routes}</MuiSideDrawer>
       </div>
     );
