350a4d303df0452fbfe326004677fdffc982f46d
pedrorvc <pedrorvc@gmail.com>
2020-07-29 20:43:41 +0100

    Added citations and engagement badges about chewBBACA to the About page.

--- frontend_react/chewie_ns/src/containers/About/About.js
+++ frontend_react/chewie_ns/src/containers/About/About.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import axios from "../../axios-backend";
+import ReactDependentScript from "react-dependent-script";
 
 // Chewie local imports
+import Aux from "../../hoc/Aux/Aux";
 import text from "../../components/data/about_md";
 import classes from "./About.module.css";
 import Markdown from "../../components/Markdown/Markdown";
@@ -11,10 +13,60 @@ import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 class About extends Component {
   render() {
     return (
-      <div id="homeDiv" className={classes.homeDiv}>
-        <Markdown markdown={text} />
-        <Copyright />
-      </div>
+      <Aux>
+        <ReactDependentScript
+          loadingComponent={<div></div>}
+          scripts={["https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js"]}
+        >
+          <div></div>
+        </ReactDependentScript>
+        <ReactDependentScript
+          loadingComponent={<div></div>}
+          scripts={["https://badge.dimensions.ai/badge.js"]}
+        >
+          <div></div>
+        </ReactDependentScript>
+
+        <div id="homeDiv" className={classes.homeDiv}>
+          <Markdown markdown={text} />
+          <Copyright />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "40px",
+          }}
+        >
+          <h3>chewBBACA metrics:</h3>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            id="engagement"
+            class="altmetric-embed"
+            data-link-target="_blank"
+            data-badge-type="medium-donut"
+            data-badge-details="right"
+            data-pmid="29543149"
+          ></div>
+          <div
+            id="citations"
+            class="__dimensions_badge_embed__"
+            data-id="pub.1101538024"
+            data-legend="always"
+            style={{ marginLeft: "2%", marginBottom: "4%" }}
+          ></div>
+        </div>
+      </Aux>
     );
   }
 }
