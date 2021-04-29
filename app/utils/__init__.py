dec0065cdb06e52322a5474b0440a20083ba3f79
pedrorvc <pedrorvcerqueira@gmail.com>
2021-04-29 13:32:24 +0100

    Moved files.

--- app/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Placeholder for automatic docs generation
-"""
--- frontend_react/chewie_ns/src/axios-backend.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-   baseURL: 'https://chewbbaca.online/api/NS/api',
-   headers: { 'Content-Type': 'application/json' },
-});
-
-export default instance;
