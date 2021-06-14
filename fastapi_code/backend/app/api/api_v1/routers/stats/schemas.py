7d3fd9a34de6f113fb861dddbce1ceae3ff0e291
pedrorvc <pedrorvcerqueira@gmail.com>
2021-06-14 16:36:04 +0100

    Wrong branch.

--- fastapi_code/backend/app/api/api_v1/routers/stats/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-import typing as t
-
-# from enum import Enum
-
-
-class SummaryResult(BaseModel):
-    result: t.List[t.Dict[str, t.Dict[str, str]]]
--- fastapi_code/scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-docker-compose run backend pytest
-docker-compose run frontend test
